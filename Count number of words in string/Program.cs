d43fe3ff80c6a2a1e3c54e29ea6a919e17988520
Sachin Gurung <saching061@hotmail.com>
2019-12-10 22:55:14 -0800

    first commit

--- Count number of words in string/Program.cs
+++ Count number of words in string/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Count_number_of_words_in_string
+public class Exercise5
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string str;
+        int i, wrd, l;
+
+        Console.Write("\n\nCount the total number of words in a string :\n");
+        Console.Write("------------------------------------------------------\n");
+        Console.Write("Input the string : ");
+        str = Console.ReadLine();
+
+        l = 0;
+        wrd = 1;
+
+        /* loop till end of string */
+        while (l <= str.Length - 1)
         {
+            /* check whether the current character is white space or new line or tab character*/
+            if (str[l] == ' ' || str[l] == '\n' || str[l] == '\t')
+            {
+                wrd++;
+            }
+
+            l++;
         }
+
+        Console.Write("Total number of words in the string is : {0}\n", wrd);
     }
 }
+/*
+Count the total number of words in a string :
+------------------------------------------------------
+Input the string : today is not saturday
+Total number of words in the string is : 4
+Press any key to continue . . .
+
+*/
